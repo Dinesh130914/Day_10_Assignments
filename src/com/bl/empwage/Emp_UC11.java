c4270c5edc3d7853a19c8dcbadd82143ab46118b
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-14 16:57:22 +0530

    Added :UC11-using interface

--- src/com/bl/empwage/Emp_UC11.java
+++ src/com/bl/empwage/Emp_UC11.java
@@ -1,5 +1,13 @@
 package com.bl.empwage;
 
-public class Emp_UC11 {
-
+public class Emp_UC11 
+{
+	public static void main(String[] args) 
+	{
+		Compnay hcl = new Compnay("HCL", 8, 20, 10, 100);
+		System.out.println("Company Name"+" "+hcl.companyName+"\n"+"Wage"+hcl.totalEmpWage);
+		
+		Compnay wipro = new Compnay("Wipro", 6, 30, 20, 50);
+		System.out.println("Companyname"+" "+wipro.companyName+"\n"+"Wage"+wipro.totalEmpWage);
+	}
 }
