47044f6eb382cb0b19d00482758bced4d75ae30c
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-15 00:40:08 +0530

    Added :Add new contacts UC2

---
+++ src/com/bl/addressbook/AddnewcontactUC2.java
@@ -0,0 +1,49 @@
+package com.bl.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddnewcontactUC2 {
+	public  void getdetails()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Details :");
+		
+		System.out.println("Enter the first name");
+		String fname = sc.next();
+		System.out.println("Enter the lastename");
+		String lname  = sc.next();
+		System.out.println("Enter the address");
+		String address = sc.next();
+		System.out.println("Enter the state");
+		String state = sc.next();
+		System.out.println("Enter the zipcode");
+		String zipcode  = sc.next();
+		System.out.println("Enter the contact number");
+		String contctnum = sc.next();
+		System.out.println("Enter the mail id");
+		String email = sc.next();
+		sc.close();
+		
+		ArrayList<String> list = new ArrayList<>();
+		list.add(fname);
+		list.add(lname);
+		list.add(address);
+		list.add(state);
+		list.add(zipcode);
+		list.add(contctnum);
+		list.add(email);
+		
+		Object[] abjarr = list.toArray();
+		for (Object object : abjarr)
+		{
+			System.out.println((String)object);
+		}
+		System.out.println("Contact Added");
+	}
+	public static void main(String[] args)
+	{
+		AddnewcontactUC2 add = new AddnewcontactUC2();
+		add.getdetails();
+	}
+}
